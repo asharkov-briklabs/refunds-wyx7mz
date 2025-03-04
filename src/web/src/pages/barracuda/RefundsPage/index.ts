5a9e0b98d2f5511b8be8d5e4f30b5b3d93b80a45
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:36:24 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/pages/barracuda/RefundsPage/index.ts

---
+++ src/web/src/pages/barracuda/RefundsPage/index.ts
@@ -0,0 +1,4 @@
+import RefundsPage from './RefundsPage'; // src/web/src/pages/barracuda/RefundsPage/RefundsPage.tsx
+
+// IE3: Export the RefundsPage component for use in routing and navigation
+export default RefundsPage;
