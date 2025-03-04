54815b31292cc63d17841f42ee638d9f09d08de6
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:35:44 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/pages/barracuda/AnalyticsPage/index.ts

---
+++ src/web/src/pages/barracuda/AnalyticsPage/index.ts
@@ -0,0 +1,4 @@
+import React from 'react'; // React v18.2.0+
+import AnalyticsPage from './AnalyticsPage';
+// IE3: Export the AnalyticsPage component for use in application routing
+export default AnalyticsPage;
