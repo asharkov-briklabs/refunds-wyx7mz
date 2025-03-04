1851b784099e4af47aec86c22641a880de13d1df
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:37:38 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/pages/pike/RefundsListPage/index.ts

---
+++ src/web/src/pages/pike/RefundsListPage/index.ts
@@ -0,0 +1,8 @@
+import RefundsListPage from './RefundsListPage';
+
+/**
+ * Export file for the RefundsListPage component used in the Pike (merchant-facing) interface. 
+ * This file re-exports the main component to simplify imports in other parts of the application, 
+ * particularly for routing configuration.
+ */
+export default RefundsListPage;
