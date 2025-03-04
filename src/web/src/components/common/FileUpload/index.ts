2f8e69837535a1b46b534ffbaff40f4a117949b6
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:32:15 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/common/FileUpload/index.ts

---
+++ src/web/src/components/common/FileUpload/index.ts
@@ -0,0 +1,3 @@
+// Export the FileUpload component and its related types for use throughout the application
+export { default } from './FileUpload';
+export { FileUploadProps, FileWithPreview } from './FileUpload';
