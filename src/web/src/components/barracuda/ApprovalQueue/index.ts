e8f99e30e62019708a6373b9d3c261bc1fe5ee28
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:30:07 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/barracuda/ApprovalQueue/index.ts

---
+++ src/web/src/components/barracuda/ApprovalQueue/index.ts
@@ -0,0 +1,5 @@
+// LD1: Import the ApprovalQueue component for re-export
+import ApprovalQueue from './ApprovalQueue';
+
+// LD1: Expose the ApprovalQueue component for use in the Barracuda admin interface
+export default ApprovalQueue;
