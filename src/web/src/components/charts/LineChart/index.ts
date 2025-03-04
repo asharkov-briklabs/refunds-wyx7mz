eede456c68822b5de5bfae7540d7d13f6c5a596b
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:30:58 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/charts/LineChart/index.ts

---
+++ src/web/src/components/charts/LineChart/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Export file for the LineChart component which is used for visualizing 
+ * time-series data like refund volumes, processing times, and other metrics
+ * in the refund service dashboards.
+ */
+
+export { default } from './LineChart';
+export * from './LineChart';
