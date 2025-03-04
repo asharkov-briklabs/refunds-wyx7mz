73c5a747e4c4746c703bb592a85c14162f079b8b
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:30:40 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/barracuda/RefundDashboard/index.ts

---
+++ src/web/src/components/barracuda/RefundDashboard/index.ts
@@ -0,0 +1,10 @@
+// src/web/src/components/barracuda/RefundDashboard/index.ts
+import RefundDashboard from './RefundDashboard'; // Import the RefundDashboard component for re-export
+
+/**
+ * Export the RefundDashboard component as the default export for this directory
+ * @component
+ * @name RefundDashboard
+ * @type React.FC
+ */
+export default RefundDashboard;
