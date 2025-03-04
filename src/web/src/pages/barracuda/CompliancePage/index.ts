b6e20c591dc6630c829d3f6b0861829a8df0247f
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:35:49 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/pages/barracuda/CompliancePage/index.ts

---
+++ src/web/src/pages/barracuda/CompliancePage/index.ts
@@ -0,0 +1,5 @@
+// src/web/src/pages/barracuda/CompliancePage/index.ts
+import React from 'react'; // react ^18.2.0
+import CompliancePage from './CompliancePage';
+// LD1: Export the CompliancePage component for use in application routing
+export default CompliancePage;
