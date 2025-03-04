7b4d3573230e6237ba61cebcfe99803b55649028
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:21:21 +0200

    Add public-samples/refunds-wyx7mz/infrastructure/terraform/modules/networking/outputs.tf

---
+++ infrastructure/terraform/modules/networking/outputs.tf
@@ -0,0 +1,75 @@
+# VPC outputs
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+# Subnet outputs
+output "public_subnet_ids" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "public_subnet_cidr_blocks" {
+  description = "List of CIDR blocks of public subnets"
+  value       = aws_subnet.public[*].cidr_block
+}
+
+output "private_subnet_cidr_blocks" {
+  description = "List of CIDR blocks of private subnets"
+  value       = aws_subnet.private[*].cidr_block
+}
+
+# Security group outputs
+output "app_security_group_id" {
+  description = "ID of the application security group"
+  value       = aws_security_group.app.id
+}
+
+output "db_security_group_id" {
+  description = "ID of the database security group"
+  value       = aws_security_group.db.id
+}
+
+output "alb_security_group_id" {
+  description = "ID of the application load balancer security group"
+  value       = aws_security_group.alb.id
+}
+
+# Availability zone outputs
+output "availability_zones" {
+  description = "List of availability zones used"
+  value       = var.availability_zones
+}
+
+# Gateway outputs
+output "nat_gateway_ids" {
+  description = "List of NAT Gateway IDs"
+  value       = aws_nat_gateway.main[*].id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+# Route table outputs
+output "route_table_public_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "route_table_private_ids" {
+  description = "List of IDs of the private route tables"
+  value       = aws_route_table.private[*].id
+}
