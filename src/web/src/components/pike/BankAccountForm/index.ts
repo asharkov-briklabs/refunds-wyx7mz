c626c57afce23c5a7a06b344ff55f8ee4950c947
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:33:57 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/pike/BankAccountForm/index.ts

---
+++ src/web/src/components/pike/BankAccountForm/index.ts
@@ -0,0 +1,12 @@
+import React from 'react'; // ^18.2.0
+import BankAccountForm, { BankAccountFormProps } from './BankAccountForm';
+
+/**
+ * Default export of the BankAccountForm component
+ */
+export default BankAccountForm;
+
+/**
+ * Re-export the props interface for the BankAccountForm component
+ */
+export type { BankAccountFormProps };
