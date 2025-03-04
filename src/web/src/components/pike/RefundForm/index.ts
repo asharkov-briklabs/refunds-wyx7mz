3dd0ffa1a4229e7a8363f6191bb74e07db592945
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:34:17 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/pike/RefundForm/index.ts

---
+++ src/web/src/components/pike/RefundForm/index.ts
@@ -0,0 +1,15 @@
+# src/web/src/components/pike/RefundForm/index.ts
+```typescript
+import React from 'react'; // react ^18.2.0
+import RefundForm from './RefundForm';
+import { RefundFormProps } from './RefundForm';
+
+/**
+ * Re-export the RefundForm component as the default export
+ */
+export default RefundForm;
+
+/**
+ * Export the props interface for external usage
+ */
+export type { RefundFormProps };
