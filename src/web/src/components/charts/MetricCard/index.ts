22b0641d818f9eb844f14029b99a106a125bed0e
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:31:03 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/charts/MetricCard/index.ts

---
+++ src/web/src/components/charts/MetricCard/index.ts
@@ -0,0 +1,5 @@
+// Re-export the MetricCard component and props interface for consistent imports
+import MetricCard, { MetricCardProps } from './MetricCard';
+
+export default MetricCard;
+export { MetricCardProps };
