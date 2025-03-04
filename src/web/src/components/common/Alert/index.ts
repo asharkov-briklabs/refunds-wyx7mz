c2ea5ead05feb30a10456a594a8f86e1f0fe83e0
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:31:17 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/common/Alert/index.ts

---
+++ src/web/src/components/common/Alert/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Exports the Alert component and its related types for easy importing
+ * throughout the application.
+ */
+
+export { default } from './Alert';
+export type { AlertProps, NotificationIconType } from './Alert';
