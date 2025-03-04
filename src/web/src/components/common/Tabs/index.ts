641572b534fec005bca1d8e1846ed682d7c5c012
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:32:58 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/common/Tabs/index.ts

---
+++ src/web/src/components/common/Tabs/index.ts
@@ -0,0 +1,11 @@
+// Re-export component and types
+export { default } from './Tabs';
+export { 
+  TabOrientation, 
+  TabSize, 
+  TabVariant 
+} from './Tabs';
+export type { 
+  TabItem, 
+  TabsProps 
+} from './Tabs';
