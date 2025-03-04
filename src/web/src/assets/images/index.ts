599b5d3566be1dc902a55a5038f45d9fe314f638
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:29:58 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/assets/images/index.ts

---
+++ src/web/src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import emptyState from './empty-state.svg';
+import errorState from './error-state.svg';
+import logo from './logo.svg';
+import logoSmall from './logo-small.svg';
+
+export {
+  emptyState,
+  errorState,
+  logo,
+  logoSmall
+};
