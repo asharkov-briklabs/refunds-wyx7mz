556d3a48ef17efc613c8676751719bfe5d526f5f
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:34:22 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/pike/RefundList/index.ts

---
+++ src/web/src/components/pike/RefundList/index.ts
@@ -0,0 +1,4 @@
+import React from 'react'; // ^18.2.0
+import RefundList from './RefundList';
+// LD1: Export the RefundList component as the default export to simplify imports from other components
+export default RefundList;
