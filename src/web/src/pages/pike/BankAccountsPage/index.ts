430ed25a36d6bf0a2bfbceb64a3de4c1e9123649
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:37:07 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/pages/pike/BankAccountsPage/index.ts

---
+++ src/web/src/pages/pike/BankAccountsPage/index.ts
@@ -0,0 +1,4 @@
+import React from 'react'; // React, version ^18.2.0
+import BankAccountsPage from './BankAccountsPage';
+// IE3: Export the BankAccountsPage component for routing
+export default BankAccountsPage;
