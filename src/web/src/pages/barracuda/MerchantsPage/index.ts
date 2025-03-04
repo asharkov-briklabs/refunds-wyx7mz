2a144c59ab843e79e34ea7fd340d4a5a4484950a
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:36:14 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/pages/barracuda/MerchantsPage/index.ts

---
+++ src/web/src/pages/barracuda/MerchantsPage/index.ts
@@ -0,0 +1,5 @@
+// LD3: Import the MerchantsPage component from its source file
+import MerchantsPage from './MerchantsPage';
+
+// LD3: Export the MerchantsPage component as the default export of this module
+export default MerchantsPage;
