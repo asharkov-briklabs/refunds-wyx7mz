71a9eb5630637ed6667b3ac154e697cad6a07ce0
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:35:54 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/pages/barracuda/ConfigurationPage/index.ts

---
+++ src/web/src/pages/barracuda/ConfigurationPage/index.ts
@@ -0,0 +1,4 @@
+import React from 'react'; // react ^18.2.0
+import ConfigurationPage from './ConfigurationPage';
+// LD1: Export the ConfigurationPage component for use in application routing
+export default ConfigurationPage;
