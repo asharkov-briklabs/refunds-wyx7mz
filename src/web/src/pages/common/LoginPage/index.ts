a2b186e310d6ba02adeb9b02dab9bc6fe06bf9b0
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:36:50 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/pages/common/LoginPage/index.ts

---
+++ src/web/src/pages/common/LoginPage/index.ts
@@ -0,0 +1,12 @@
+import LoginPage from './LoginPage'; // Import the LoginPage component from its file
+
+/**
+ * @file Exports the LoginPage component for authentication to both Pike (merchant) and Barracuda (admin) interfaces
+ */
+
+/**
+ * @description Re-exports the LoginPage component as the default export from this index file
+ * @exports LoginPage
+ * @type {React.FC}
+ */
+export default LoginPage;
