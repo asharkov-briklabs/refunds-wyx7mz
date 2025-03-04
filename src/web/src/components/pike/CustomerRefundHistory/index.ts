37ec3f5b86fe502bacab457ade6c0870ebc8470b
Alexander Sharkov <143603362+asharkov-briklabs@users.noreply.github.com>
2025-03-04 11:34:07 +0200

    Add public-samples/refunds-wyx7mz/src/web/src/components/pike/CustomerRefundHistory/index.ts

---
+++ src/web/src/components/pike/CustomerRefundHistory/index.ts
@@ -0,0 +1,8 @@
+// LD1: Import the CustomerRefundHistory component for re-export
+import CustomerRefundHistory from './CustomerRefundHistory';
+
+// LD1: Export the CustomerRefundHistory component as the default export
+export default CustomerRefundHistory;
+
+// LD1: Re-export any named exports from the CustomerRefundHistory component
+export * from './CustomerRefundHistory';
